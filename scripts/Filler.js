36be8b6a9f51426541c858ffc5a0480bb91ccdb4
sadrawn <146434307+sadrawn@users.noreply.github.com>
2024-05-08 09:12:50 +0330

    setting start

--- scripts/Filler.js
+++ scripts/Filler.js
@@ -1,95 +1,178 @@
 
 let Header = `
-    <div class="mobile">
+        <div class="mobile">
 
-    <!-- mobile version  -->
-    <div class="mobileHeader">
-        <div class="hamburgerMenu">
-            <img src="resource/Images/icons/icons8-menu-50 (2).png" alt="">
+        <!-- mobile version  -->
+        <div class="mobileHeader">
+            <div class="hamburgerMenu">
+                <img src="resource/Images/icons/icons8-menu-50 (2).png" alt="">
+            </div>
+            <div class="name">
+                نظرسنجی هنرسان مصطفی خمینی
+            </div>
         </div>
-        <div class="name">
-            نظرسنجی هنرسان مصطفی خمینی
-        </div>
-    </div>
 
-    <!-- header card -->
-    <div class=" downSide downsideMobile">
-        <img src="resource/Images/6770965.jpg" alt="">
-        <div class="Header-text">
-            <h2>
-                به سایت نظرسنجی هنرستان مصطفی خمینی خوش آمدید.
-            </h2>
-            <p>
-                ما با افتخار اعلام می‌کنیم که به عنوان یکی از موسسات آموزشی برجسته در سال تحصیلی جاری، به پیشبرد
-                آموزش و پرورش با استانداردهای بالا متعهد هستیم. با تمرکز بر ارائه برنامه‌های آموزشی کارآمد و
-                ایجاد محیطی پویا و فراگیر، ما به ارتقاء عملکرد تحصیلی و رشد هرچه بیشتر دانش‌آموزان می‌پردازیم.
-                ارزیابی نظرات شما ارزشمند است و به ما کمک می‌کند تا بهبودهای لازم را ایجاد کرده و به بهترین شکل
-                ممکن خدمات را ارائه دهیم. با تشکر از شما برای همراهی در این مسیر.
-            </p>
-            <a href="Querypage.html">
-                <button>شرکت در نظرسجی</button>
+        <!-- header card -->
+        <div class=" downSide downsideMobile">
+            <img src="resource/Images/6770965.jpg" alt="">
+            <div class="Header-text">
+                <h2>
+                    به سایت نظرسنجی هنرستان مصطفی خمینی خوش آمدید.
+                </h2>
+                <p>
+                    ما با افتخار اعلام می‌کنیم که به عنوان یکی از موسسات آموزشی برجسته در سال تحصیلی جاری، به
+                    پیشبرد
+                    آموزش و پرورش با استانداردهای بالا متعهد هستیم. با تمرکز بر ارائه برنامه‌های آموزشی کارآمد و
+                    ایجاد محیطی پویا و فراگیر، ما به ارتقاء عملکرد تحصیلی و رشد هرچه بیشتر دانش‌آموزان
+                    می‌پردازیم.
+                    ارزیابی نظرات شما ارزشمند است و به ما کمک می‌کند تا بهبودهای لازم را ایجاد کرده و به بهترین
+                    شکل
+                    ممکن خدمات را ارائه دهیم. با تشکر از شما برای همراهی در این مسیر.
+                </p>
+                <a href="Querypage.html">
+                    <button>شرکت در نظرسجی</button>
+                </a>
+            </div>
+        </div>
+        <!-- ? header drop down -->
+        <div class="HeaderDropDown">
+            <div class="upperDown">
+                <p>تنظیمات بیشتر</p>
+                <img class="exit" src="resource/Images/icons/icons8-cross-48.png" alt="">
+            </div>
+            <div class="downHead">
+                <ul>
+                    <!-- home -->
+                    <a href="index.html">
+                        <li>
+                            <img src="resource/Images/icons/header/icons8-home-50.png" alt="">
+                            <p>صفحه اصلی</p>
+                        </li>
+                    </a>
+                    <!-- about us  -->
+                    <a href="AboutUs.html">
+                        <li>
+                            <img src="resource/Images/icons/header/icons8-about-us-50.png" alt="">
+                            <p>درباره ما</p>
+                        </li>
+                    </a>
+                    <!-- contact as -->
+                    <a href="contact.html">
+                        <li>
+                            <img src="resource/Images/icons/header/icons8-contact-us-50.png" alt="">
+                            <p>ارتباط با ما</p>
+                        </li>
+                    </a>
+                    <!-- sign up -->
+                    <a href="signUp.html" class="signup">
+                        <li>
+                            <img src="resource/Images/icons/header/icons8-sign-up-32.png" alt="">
+                            <p>ثبت نام</p>
+                        </li>
+                    </a>
+                    <!-- login -->
+                    <a href="Login.html" class="login">
+                        <li>
+                            <img src="resource/Images/icons/header/icons8-login-50.png" alt="">
+                            <p>ورود</p>
+                        </li>
+                    </a>
+                    <!-- log out -->
+                    <a href="index.html" class="logout">
+                        <li>
+                            <img src="resource/Images/icons/header/icons8-log-out-50.png" alt="">
+                            <p>خروج</p>
+                        </li>
+                    </a>
+
+                </ul>
+                <hr>
+                <div class="userBar">
+                    <ul>
+                        <!-- Setting -->
+                        <a href="Setting.html">
+                            <li>
+                                <img src="resource/Images/icons/header/icons8-setting-50.png" alt="">
+                                <p>تنظیمات</p>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+
+
+        <!--  ! pc version -->
+        <div class="pc">
+        <div class="upperSide">
+            <nav>
+                <ul>
+                    <a href="index.html">
+                        <li>
+                            <p>صفحه اصلی</p>
+                        </li>
+                    </a>
+                    <!-- about us  -->
+                    <a href="AboutUs.html">
+                        <li>
+                            <p>درباره ما</p>
+                        </li>
+                    </a>
+                    <!-- contact as -->
+                    <a href="Contact.html">
+                        <li>
+                            <p>ارتباط با ما</p>
+                        </li>
+                    </a>
+                    <!-- sign up -->
+                    <a href="signUp.html" class="signup">
+                        <li>
+                            <p>ثبت نام</p>
+                        </li>
+                    </a>
+                    <!-- login -->
+                    <a href="Login.html" class="login">
+                        <li>
+                            <p>ورود</p>
+                        </li>
+                    </a>
+                    <a href="Querypage.html" class="participate login">
+                        <li>
+                            <p>شرکت در نظرسنجی</p>
+                        </li>
+                    </a>
+                </ul>
+            </nav>
+            <a href="Setting.html">
+                <div class="account">
+                    <img src="resource/Images/icons/account-50 (1).png" alt="profile image">
+                    <p name="username">وارد شوید</p>
+                </div>
             </a>
         </div>
-    </div>
-    <!-- ? header drop down -->
-    <div class="HeaderDropDown">
-        <div class="upperDown">
-            <p>تنظیمات بیشتر</p>
-            <img class="exit" src="resource/Images/icons/icons8-cross-48.png" alt="">
+        <div class="downSide">
+            <img src="resource/Images/6770965.jpg" alt="">
+            <div class="Header-text">
+                <h2>
+                    به سایت نظرسنجی هنرستان مصطفی خمینی خوش آمدید.
+                </h2>
+                <p>
+                    ما با افتخار اعلام می‌کنیم که به عنوان یکی از موسسات آموزشی برجسته در سال تحصیلی جاری، به
+                    پیشبرد
+                    آموزش و پرورش با استانداردهای بالا متعهد هستیم. با تمرکز بر ارائه برنامه‌های آموزشی کارآمد و
+                    ایجاد محیطی پویا و فراگیر، ما به ارتقاء عملکرد تحصیلی و رشد هرچه بیشتر دانش‌آموزان
+                    می‌پردازیم.
+                    ارزیابی نظرات شما ارزشمند است و به ما کمک می‌کند تا بهبودهای لازم را ایجاد کرده و به بهترین
+                    شکل
+                    ممکن خدمات را ارائه دهیم. با تشکر از شما برای همراهی در این مسیر.
+                </p>
+                <a href="#">
+                    <button>شرکت در نظرسجی</button>
+                </a>
+            </div>
         </div>
-        <div class="downHead">
-            <ul>
-                <li><a href="index.html" class="active">صفحه اصلی</a></li>
-                <li><a href="AboutUs.html">درباره ما </a></li>
-                <li><a href="Cotact.html">ارتباط با ما </a></li>
-                <li><a href="signUp.html">ثبت نام</a></li>
-                <li><a href="Login.html">ورود</a></li>
-                <li><a href="Querypage.html">شرکت در نظرسنجی</a></lّi>
-            </ul>
         </div>
-    </div>
-    </div>
-
-
-    <!--  ! pc version -->
-    <div class="pc">
-    <div class="upperSide">
-        <nav>
-            <ul>
-            <li><a href="index.html" class="active">صفحه اصلی</a></li>
-            <li><a href="AboutUs.html">درباره ما </a></li>
-            <li><a href="Cotact.html">ارتباط با ما </a></li>
-            <li><a href="signUp.html">ثبت نام</a></li>
-            <li><a href="Login.html">ورود</a></li>
-            <li><a href="Querypage.html" class="participate">شرکت در نظرسنجی</a></lّi>
-            </ul>
-        </nav>
-        <a href="UserPage.html">
-        <div class="account">
-            <img src="resource/Images/icons/account-50 (1).png" alt="profile image">
-            <p name="username">مهمان</p>
-        </div>
-        </a>
-    </div>
-    <div class="downSide">
-        <img src="resource/Images/6770965.jpg" alt="">
-        <div class="Header-text">
-            <h2>
-                به سایت نظرسنجی هنرستان مصطفی خمینی خوش آمدید.
-            </h2>
-            <p>
-                ما با افتخار اعلام می‌کنیم که به عنوان یکی از موسسات آموزشی برجسته در سال تحصیلی جاری، به پیشبرد
-                آموزش و پرورش با استانداردهای بالا متعهد هستیم. با تمرکز بر ارائه برنامه‌های آموزشی کارآمد و
-                ایجاد محیطی پویا و فراگیر، ما به ارتقاء عملکرد تحصیلی و رشد هرچه بیشتر دانش‌آموزان می‌پردازیم.
-                ارزیابی نظرات شما ارزشمند است و به ما کمک می‌کند تا بهبودهای لازم را ایجاد کرده و به بهترین شکل
-                ممکن خدمات را ارائه دهیم. با تشکر از شما برای همراهی در این مسیر.
-            </p>
-            <a href="#">
-                <button>شرکت در نظرسجی</button>
-            </a>
-        </div>
-    </div>
-    </div>
 `;
 
 export function HeaderFiller() {
