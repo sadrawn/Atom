323a9c4972524d9f71f85945b1a8b5ee9a6597c8
sadrawn <146434307+sadrawn@users.noreply.github.com>
2024-05-09 10:30:17 +0330

    SIgnUp finished

--- scripts/signUpScript.js
+++ scripts/signUpScript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const nextBtn = document.querySelector(".nextBtn");
 const pass = document.querySelector(".pass");
 const confirmation = document.querySelector(".confirmation");
@@ -38,24 +37,18 @@ nextBtn.addEventListener("click", (e) => {
     confirmationErrorP.stye.display = "none";
   }
 });
-=======
-const nextBtn = document.querySelector(".nextBtn");
-const pass = document.querySelector(".pass");
-const confirmation = document.querySelector(".confirmation");
-const Gcode = document.querySelector(".Gcode");
-const confirmationErrorP = document.querySelector(".confirmationErrorP");
-const GcodeP = document.querySelector(".GcodeP");
-const email = document.querySelector(".email");
-const name = document.querySelector(".name");
+
+// const nextBtn = document.querySelector(".nextBtn");
+// const pass = document.querySelector(".pass");
+// const confirmation = document.querySelector(".confirmation");
+// const Gcode = document.querySelector(".Gcode");
+// const confirmationErrorP = document.querySelector(".confirmationErrorP");
+// const GcodeP = document.querySelector(".GcodeP");
+// const email = document.querySelector(".email");
+// const name = document.querySelector(".name");
 
 nextBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  // check Gcode
-  if (Gcode.value.length < 10) {
-    GcodeP.textContent = "کد ملی نمی تواند کمتر از 10 کاراکتر باشد.";
-  } else {
-    GcodeP.style.display = "none";
-  }
   // check value of inputs
   if (
     pass.value == "" ||
@@ -78,4 +71,3 @@ nextBtn.addEventListener("click", (e) => {
     confirmationErrorP.stye.display = "none";
   }
 });
->>>>>>> 6a4e5a403da6f42c76267d4c9a060db9bae4e02a
