94459c2f4eee03bd5a066c6059b0dd4732b4a612
sadrawn <146434307+sadrawn@users.noreply.github.com>
2024-06-09 21:05:30 +0330

    signUp

---
+++ scripts/SurveyPage.js
@@ -0,0 +1,6 @@
+// for header and footer 
+import { HeaderFiller, FooterFiller } from './Filler.js';
+import { asideActivator } from './AsideActivator.js';
+HeaderFiller();
+FooterFiller();
+asideActivator();
